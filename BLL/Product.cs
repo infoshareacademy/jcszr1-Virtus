53c48bebe3f375ef1698919a576c1f862580c5e7
Marcin Rybiński <rybinski.marcin@gmail.com>
2020-07-14 17:32:04 +0200

    Product.cs added

---
+++ BLL/Product.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace BLL
+{
+    public class Product
+    {
+        private int productId;
+        private string productName;
+        private int productWeight;
+        private int energy;
+        private double fat;
+        private double saturatesInFat;
+        private double carbonhydrates;
+        private double sugarsInCarbonhydrates;
+        private double protein;
+        private double salt;
+        private bool isVegetarian;
+    }
+}
