b9e6acf986d7c8e2a54fc20ee488074a0d7b6549
Marcin Rybiński <rybinski.marcin@gmail.com>
2020-07-28 19:42:57 +0200

    added SearchProduct.css in BLL and DisplayProduct.cs

---
+++ BLL/SearchProduct.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using BLL;
+
+
+namespace VirtusFitConsole
+{
+    class SearchProduct
+    {
+        public List<Product> SearchByName(List<Product> productList, string searchValue)
+        {
+            var returnList = new List<Product>();
+
+            //return productList.Where(x => x.ProductName.Contains(searchValue, StringComparison.InvariantCultureIgnoreCase)).ToList();
+
+            foreach (var product in productList)
+            {
+                if (product.ProductName.Contains(searchValue, StringComparison.InvariantCultureIgnoreCase))
+                {
+                    returnList.Add(product);
+                }
+            }
+            return returnList;
+        }
+        public List<Product> SearchByCalories(List<Product> productList, int searchValue)
+        {
+
+            var returnList = new List<Product>();
+
+            foreach (var product in productList)
+            {
+                if (product.Energy == searchValue)
+                {
+                    returnList.Add(product);
+                }
+            }
+            return returnList;
+        }
+
+        public List<Product> SearchByCalories(List<Product> productList, int minValue, int maxValue)
+        {
+            var returnList = new List<Product>();
+
+            foreach (var product in productList)
+            {
+                if (product.Energy >= minValue && product.Energy <= maxValue)
+                {
+                    returnList.Add(product);
+                }
+            }
+            return returnList;
+        }
+    }
+}
---
+++ VirtusFitConsole/DisplayProductList.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using BLL;
+
+namespace VirtusFitConsole
+{
+    static class DisplayProductList
+    {
+        public static void DisplayList(List<Product> _productList)
+        {
+
+
+            Console.WriteLine();
+            Console.WriteLine("Id".PadRight(4) + "Name".PadRight(15) + "Energy".PadRight(10) + "Fat".PadRight(8)
+                              + "Saturates".PadRight(10) + "Carbohydrates".PadRight(14) + "Sugars In".PadRight(14)
+                              + "Protein".PadRight(8) + "Salt".PadRight(8) + "Vegetarian".PadRight(8));
+            Console.WriteLine("".PadRight(4) + "".PadRight(15) + "".PadRight(10) + "".PadRight(8) + "In Fat".PadRight(10)
+                              + "".PadRight(14) + "Carbohydrates".PadRight(14) + "".PadRight(8) + "".PadRight(8) + "".PadRight(8));
+            Console.WriteLine("All values refer to 100g portion.");
+            for (int i = 0; i < 95; i++) Console.Write("#");
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            foreach (var product in _productList)
+            {
+                Console.WriteLine((product.ProductId + ".").PadRight(4) + product.ProductName.PadRight(15)
+                                                                        + (product.Energy + "kcal").PadRight(10) + (product.Fat + "g").PadRight(8)
+                                                                        + (product.SaturatesInFat + "g").PadRight(8) + (product.Carbohydrates + "g").PadRight(13)
+                                                                        + (product.SugarsInCarbohydrates + "g").PadRight(13) + (product.Protein + "g").PadRight(8)
+                                                                        + (product.Salt + "g").PadRight(8) + (product.IsVegetarian + "g").PadRight(8));
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
