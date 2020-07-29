29a627284aff8d180e98edf579501299baa7938d
Marcin Rybiński <rybinski.marcin@gmail.com>
2020-07-29 19:31:03 +0200

    changed variables names in DisplayProductList.cs

--- BLL/SearchProduct.cs
+++ BLL/SearchProduct.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using BLL;
 
-
-namespace VirtusFitConsole
+namespace BLL
 {
     public class SearchProduct
     {
--- VirtusFitConsole/DisplayProductList.cs
+++ VirtusFitConsole/DisplayProductList.cs
@@ -6,7 +6,7 @@ namespace VirtusFitConsole
 {
     static class DisplayProductList
     {
-        public static void DisplayList(List<Product> _productList)
+        public static void DisplayList(List<Product> productList)
         {
 
 
@@ -17,12 +17,12 @@ namespace VirtusFitConsole
             Console.WriteLine("".PadRight(4) + "".PadRight(15) + "".PadRight(10) + "".PadRight(8) + "In Fat".PadRight(10)
                               + "".PadRight(14) + "Carbohydrates".PadRight(14) + "".PadRight(8) + "".PadRight(8) + "".PadRight(8));
             Console.WriteLine("All values refer to 100g portion.");
-            for (int i = 0; i < 95; i++) Console.Write("#");
+            for (var i = 0; i < 95; i++) Console.Write("#");
             Console.WriteLine();
             Console.WriteLine();
             
-            if (_productList.Count <= 0) Console.WriteLine("There are no items to display.");
-            foreach (var product in _productList)
+            if (productList.Count <= 0) Console.WriteLine("There are no items to display.");
+            foreach (var product in productList)
             {
                 Console.WriteLine((product.productId + ".").PadRight(4) + product.productName.PadRight(15)
                                                                         + (product.energy + "kcal").PadRight(10) + (product.fat + "g").PadRight(8)
