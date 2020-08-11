7843a8c5e6dcd313bc2f96e9c0eedcbf7fb45318
Krzysztof Spaliński <67453642+Valdes-C@users.noreply.github.com>
2020-08-11 22:01:04 +0200

    Satrt  commit

--- VirtusFitConsole/Program.cs
+++ VirtusFitConsole/Program.cs
@@ -11,22 +11,26 @@ namespace VirtusFitConsole
         public static List<Product> ListOfProducts = new List<Product>();
         static void Main(string[] args)
         {
-
-            try
+            int userInput = 0;
+            do
+            {
+                userInput = DisplayMenu();
+            } while (userInput != 6);
+            /*try
             {
                 foreach (var item in ProductLoader.GetProductsFromFile())
                 {
                     ListOfProducts.Add(item);
                 }
 
-                //foreach (var item in ListOfProducts)
-                //{
-                //    Console.WriteLine(item.ProductName + item.ProductId + item.PortionQuantity + item.PortionUnit + item.Energy + item.Fat + item.Fiber + item.Sugar);
-                //}
+                 foreach (var item in ListOfProducts)
+                {
+                    Console.WriteLine(item.ProductName + item.ProductId + item.PortionQuantity + item.PortionUnit + item.Energy + item.Fat + item.Fiber + item.Sugar);
+                }
 
                 DisplayProductList.DisplayList(ListOfProducts);
                 EditDataFromConsoleInterface testInterface = new EditDataFromConsoleInterface();
-                testInterface.EditProductInterface(ListOfProducts);
+                testInterface.EditProductInterface();   //EditProductInterface(ListOfProducts);
 
                 DisplayProductList.DisplayList(ListOfProducts);
             }
@@ -34,8 +38,29 @@ namespace VirtusFitConsole
             {
                 Console.WriteLine($"An error occured: {e.Message} \nPress any key.");
                 Console.ReadKey();
+            } */
+
+        }
+        static public int DisplayMenu()
+        {
+            Console.WriteLine("Wybierz opcję");
+            Console.WriteLine();
+            Console.WriteLine("1. Wyszukaj produkt po kaloryczności");
+            Console.WriteLine("2. Wyszukaj produkt po nazwie");
+            Console.WriteLine("3. Zawansowane wyszukiwanie produktu");
+            Console.WriteLine("4. Wprowadź nowy produkt");
+            Console.WriteLine("5. Stwórz plan dietetyczny");
+            Console.WriteLine("6. Zakończ");
+            
+            int result=1;
+            bool itsNumeric=false;
+            
+            while (itsNumeric == false)
+            {
+                itsNumeric = int.TryParse(Console.ReadLine().ToString(), out result);
             }
 
+            return result;
         }
     }
 }
