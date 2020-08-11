91da21269f0b2a39787ab750c0475b47ead55be2
Krzysztof Spaliński <67453642+Valdes-C@users.noreply.github.com>
2020-08-11 23:22:18 +0200

    Dopisanie menu

--- VirtusFitConsole/Program.cs
+++ VirtusFitConsole/Program.cs
@@ -14,41 +14,25 @@ namespace VirtusFitConsole
             Console.WriteLine("---------------------------------------------------");
             Console.WriteLine("Witaj w programie do tworzenia planu dietetycznego!");
             Console.WriteLine("---------------------------------------------------");
-            Console.WriteLine("");
+            Console.WriteLine();
             int userInput = 1;
             do
             {
                 userInput = DisplayMenu();
-
+                new ExecuteOption(userInput);
             } while (userInput != 6);
-            /*try
-            {
-                foreach (var item in ProductLoader.GetProductsFromFile())
-                {
-                    ListOfProducts.Add(item);
-                }
+        }
 
-                 foreach (var item in ListOfProducts)
-                {
-                    Console.WriteLine(item.ProductName + item.ProductId + item.PortionQuantity + item.PortionUnit + item.Energy + item.Fat + item.Fiber + item.Sugar);
-                }
 
-                DisplayProductList.DisplayList(ListOfProducts);
-                EditDataFromConsoleInterface testInterface = new EditDataFromConsoleInterface();
-                testInterface.EditProductInterface();   //EditProductInterface(ListOfProducts);
 
-                DisplayProductList.DisplayList(ListOfProducts);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine($"An error occured: {e.Message} \nPress any key.");
-                Console.ReadKey();
-            } */
-
+        public static bool BetweenRanges(int a, int b, int number)
+        {
+            return (a <= number && number <= b);
         }
         static public int DisplayMenu()
         {
-            Console.WriteLine("Wybierz opcję");
+            Console.WriteLine();
+            Console.WriteLine("Wybierz opcję:");
             Console.WriteLine();
             Console.WriteLine("1. Wyszukaj produkt po kaloryczności");
             Console.WriteLine("2. Wyszukaj produkt po nazwie");
@@ -56,16 +40,49 @@ namespace VirtusFitConsole
             Console.WriteLine("4. Wprowadź nowy produkt");
             Console.WriteLine("5. Stwórz plan dietetyczny");
             Console.WriteLine("6. Zakończ");
-            
+            Console.WriteLine();
+
             int _result=1;
-            bool itsNumeric=false;
-            
-            while (itsNumeric == false)
+            bool _itsNumeric;
+            _itsNumeric = false;
+
+            while (_itsNumeric == false)
             {
-                itsNumeric = int.TryParse(Console.ReadLine().ToString(), out _result);
+                _itsNumeric = int.TryParse(Console.ReadLine().ToString(), out _result);
+            }
+
+            while (!BetweenRanges(1, 6, _result))
+            {
+                _itsNumeric = int.TryParse(Console.ReadLine().ToString(), out _result);
             }
 
             return _result;
+        }
+
+        private class ExecuteOption
+        {
+            private int userInput;
+
+            public ExecuteOption(int userInput)
+            {
+                switch (userInput)
+                {
+                    case 1:
+                        new SearchProductConsoleInterface();
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        break;
+                    case 5:
+                        break;
+                    default:
+                        break;
+                }
+
+            }
         }
     }
 }
