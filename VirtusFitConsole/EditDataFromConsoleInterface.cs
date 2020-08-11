4787b624a587d699c63d1ad5791abfc4862f0dd4
Dawid Gal <d.gal@poczta.onet.pl>
2020-08-11 18:50:11 +0200

    Fixed data validation and added fix to not be able to enter lower than 0 values in editing

--- VirtusFitConsole/EditDataFromConsoleInterface.cs
+++ VirtusFitConsole/EditDataFromConsoleInterface.cs
@@ -8,7 +8,7 @@ namespace VirtusFitConsole
 {
     class EditDataFromConsoleInterface
     {
-        private static readonly string[] EditOptions = { "Product", "Energy", "Fat", "Carbohydrates", "Proteins", "Salt", "Fiber", "Sugar", "Product Quantity", "Portion quantity", "Portion unit" };
+        private static readonly string[] EditOptions = { "Product name", "Energy", "Fat", "Carbohydrates", "Proteins", "Salt", "Fiber", "Sugar", "Product quantity", "Portion quantity", "Portion unit" };
         private static int _currentLine;
 
         private string DisplayOptions(int cursorPos)
@@ -59,11 +59,15 @@ namespace VirtusFitConsole
             return "";
         }
 
-        public void EditProductInterface(List<Product> productList)
+        public void EditProductInterface()
         {
             Console.WriteLine("Provide the ID of the product that you would like to update");
             string productId = Console.ReadLine();
             var productToBeUpdated = Program.ListOfProducts.Where(product => product.ProductId.Equals(Convert.ToInt32(productId))).ToList();
+            if (productToBeUpdated[0] == null)
+            {
+                throw new ArgumentException("The product could not be found.");
+            }
             string newStringValue;
             int newIntValue;
             Console.WriteLine("What would you like to update?");
@@ -74,11 +78,11 @@ namespace VirtusFitConsole
                 userDecision = DisplayOptions(cursorPos);
             } while (userDecision != "Product name" && userDecision != "Energy" && userDecision != "Fat" && userDecision != "Carbohydrates" && userDecision != "Proteins" && userDecision != "Salt" && userDecision != "Fiber" && userDecision != "Sugar" && userDecision != "Product quantity" && userDecision != "Portion quantity" && userDecision != "Portion unit"  );
 
+            Console.Clear();
             switch (userDecision)
             {
                 case "Product name":
                     {
-                        Console.Clear();
                         Console.WriteLine("Please provide new product name: ");
                         newStringValue = Console.ReadLine();
                         productToBeUpdated[0].ProductName = newStringValue;
@@ -87,126 +91,131 @@ namespace VirtusFitConsole
                     }
                 case "Energy":
                     {
-                        Console.Clear();
                         Console.WriteLine("Please provide new energy value: ");
                         var valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
-                        if (valueEntered)
+                        while (newIntValue < 0 || valueEntered == false)
                         {
-                            productToBeUpdated[0].Energy = newIntValue;
-                            Console.WriteLine("Product energy value has been updated!");
-                            break;
+                            Console.WriteLine("The energy value must be a number higher then or equal zero");
+                            valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
                         }
-                        throw new ArgumentException("Energy value must be a valid number.");
+                        productToBeUpdated[0].Energy = newIntValue;
+                        Console.WriteLine("Product energy value has been updated!");
+                        break;
                     }
                 case "Fat":
                     {
-                        Console.Clear();
                         Console.WriteLine("Please provide new fat value: ");
                         var valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
-                        if (valueEntered)
+                        while (newIntValue < 0 || valueEntered == false)
                         {
-                            productToBeUpdated[0].Fat = newIntValue;
-                            Console.WriteLine("Product fat value has been updated!");
-                            break;
+                            Console.WriteLine("The fat value must be a number higher than or equal zero");
+                            valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
                         }
-                        throw new ArgumentException("Fat value must be a valid number.");
+                        productToBeUpdated[0].Fat = newIntValue;
+                        Console.WriteLine("Product fat value has been updated!");
+                        break;
                     }
                 case "Carbohydrates":
                     {
-                        Console.Clear();
                         Console.WriteLine("Please provide new carbohydrates value: ");
                         var valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
-                        if (valueEntered)
+                        while (newIntValue < 0 || valueEntered == false)
                         {
-                            productToBeUpdated[0].Carbohydrates = newIntValue;
-                            Console.WriteLine("Product carbohydrates value has been updated!");
-                            break;
+                            Console.WriteLine("The carbohydrates value must be a number higher than or equal zero");
+                            valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
                         }
-                        throw new ArgumentException("Carbohydrates value must be a valid number.");
+                        productToBeUpdated[0].Carbohydrates = newIntValue;
+                        Console.WriteLine("Product carbohydrates value has been updated!");
+                        break;
                     }
                 case "Protein":
                     {
-                        Console.Clear();
-                        Console.WriteLine("Please provide new proteins value: ");
+                        Console.WriteLine("Please provide new protein value: ");
                         var valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
-                        if (valueEntered)
+                        while (newIntValue < 0 || valueEntered == false)
                         {
-                            productToBeUpdated[0].Protein = newIntValue;
-                            Console.WriteLine("Product protein value has been updated!");
-                            break;
+                            Console.WriteLine("The protein value must be a number higher than or equal zero");
+                            valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
                         }
-                        throw new ArgumentException("Protein value must be a valid number.");
+                        productToBeUpdated[0].Protein = newIntValue;
+                        Console.WriteLine("Product protein value has been updated!");
+                        break;
                     }
                 case "Salt":
                     {
-                        Console.Clear();
                         Console.WriteLine("Please provide new salt value: ");
                         var valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
-                        if (valueEntered)
+                        while (newIntValue < 0 || valueEntered == false)
                         {
-                            productToBeUpdated[0].Salt = newIntValue;
-                            Console.WriteLine("Product salt value has been updated!");
-                            break;
+                            Console.WriteLine("The salt value must be a number higher than or equal zero");
+                            valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
                         }
-                        throw new ArgumentException("Salt value must be a valid number.");
+                        productToBeUpdated[0].Salt = newIntValue;
+                        Console.WriteLine("Product protein value has been updated!");
+                        break;
                     }
                 case "Fiber":
                     {
-                        Console.Clear();
                         Console.WriteLine("Please provide new fiber value: ");
                         var valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
-                        if (valueEntered)
+                        while (newIntValue < 0 || valueEntered == false)
                         {
-                            productToBeUpdated[0].Fiber = newIntValue;
-                            Console.WriteLine("Product fiber value has been updated!");
-                            break;
+                            Console.WriteLine("The fiber value must be a number higher than or equal zero");
+                            valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
                         }
-                        throw new ArgumentException("Fiber value must be a valid number.");
+                        productToBeUpdated[0].Fiber = newIntValue;
+                        Console.WriteLine("Product fiber value has been updated!");
+                        break;
                     }
                 case "Sugar":
                     {
-                        Console.Clear();
                         Console.WriteLine("Please provide new sugar value: ");
                         var valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
-                        if (valueEntered)
+                        while (newIntValue < 0 || valueEntered == false)
                         {
-                            productToBeUpdated[0].Sugar = newIntValue;
-                            Console.WriteLine("Product salt value has been updated!");
-                            break;
+                            Console.WriteLine("The salt value must be a number higher than or equal zero");
+                            valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
                         }
-                        throw new ArgumentException("Sugar value must be a valid number.");
+                        productToBeUpdated[0].Sugar = newIntValue;
+                        Console.WriteLine("Product sugar value has been updated!");
+                        break;
                     }
                 case "Product quantity":
                     {
-                        Console.Clear();
                         Console.WriteLine("Please provide new product quantity value: ");
                         var valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
-                        if (valueEntered)
+                        while (newIntValue < 0 || valueEntered == false)
                         {
-                            productToBeUpdated[0].Quantity = newIntValue;
-                            Console.WriteLine("Product product quantity value has been updated!");
-                            break;
+                            Console.WriteLine("The product quantity value must be a number higher than or equal zero");
+                            valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
                         }
-                        throw new ArgumentException("Product quantity value must be a valid number.");
+                        productToBeUpdated[0].Quantity = newIntValue;
+                        Console.WriteLine("Product protein value has been updated!");
+                        break;
                     }
                 case "Portion quantity":
                     {
-                        Console.Clear();
                         Console.WriteLine("Please provide new portion quantity value: ");
                         var valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
-                        if (valueEntered)
+                        while (newIntValue < 0 || valueEntered == false)
                         {
-                            productToBeUpdated[0].PortionQuantity = newIntValue;
-                            Console.WriteLine("Product portion quantity value has been updated!");
-                            break;
+                            Console.WriteLine("The portion quantity value must be a number higher than or equal zero");
+                            valueEntered = int.TryParse(Console.ReadLine(), out newIntValue);
                         }
-                        throw new ArgumentException("Portion quantity value must be a valid number.");
+                        productToBeUpdated[0].PortionQuantity = newIntValue;
+                        Console.WriteLine("Product portion quantity value has been updated!");
+                        break;
                     }
                 case "Portion unit":
                     {
-                        Console.Clear();
-                        Console.WriteLine("Please provide new portion unit value: ");
+                        string[] units = {"g", "ml"};
+                        Console.WriteLine("Please provide new portion unit value, g for grams or ml for mililiters");
                         newStringValue = Console.ReadLine();
+                        while (newStringValue != "g" && newStringValue != "ml")
+                        {
+                            Console.WriteLine("Please use either g or ml");
+                            newStringValue = Console.ReadLine();
+                        }
                         productToBeUpdated[0].PortionUnit = newStringValue;
                         Console.WriteLine("Product portion quantity value has been updated!");
                         break;
