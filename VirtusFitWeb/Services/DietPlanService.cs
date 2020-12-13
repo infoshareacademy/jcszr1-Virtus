59be8000aa609e062f986c9034b4b0f0ddf3b202
Marcin Rybinski <rybinski.marcin@gmail.com>
2020-12-13 21:16:32 +0100

    fixed editing

--- VirtusFitWeb/Services/DietPlanService.cs
+++ VirtusFitWeb/Services/DietPlanService.cs
@@ -104,7 +104,7 @@ namespace VirtusFitWeb.Services
             }
 
             dietPlan.DailyDietPlanList = null;
-
+            
             _dietPlanRepository.UpdateDietPlan(dietPlan);
         }
 
